74e393066dfc0ccf921ea0ffadb44f13fda18ec4
Ilkka Tommola <ilta@iki.fi>
2025-01-10 21:58:52 +0200

    Completed ex 3.3 and 3.4
    * Included a .rest file for testing deletion in VS Code

--- index.js
+++ index.js
@@ -37,6 +37,24 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((p) => p.id !== id);
+
+  response.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
